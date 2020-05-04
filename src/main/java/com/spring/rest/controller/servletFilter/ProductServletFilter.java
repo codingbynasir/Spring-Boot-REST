2ba5c04af68898080f641e4aaec04218b99047fd
Coding with nasir <nasirkhancse@gmail.com>
2020-05-04 12:28:13 +0600

    ServletFilter is added

---
+++ src/main/java/com/spring/rest/controller/servletFilter/ProductServletFilter.java
@@ -0,0 +1,26 @@
+package com.spring.rest.controller.servletFilter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+public class ProductServletFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Remote Host: "+servletRequest.getRemoteHost());
+        System.out.println("Remote Address: "+servletRequest.getRemoteAddr()+":"+servletRequest.getRemotePort());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
